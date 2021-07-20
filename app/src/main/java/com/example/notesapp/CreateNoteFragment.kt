4f6d0564ef2e65dca8d299d3636b8ab77f94b56c
vuph <46913707+vupham04@users.noreply.github.com>
2021-07-20 17:44:11 +0700

    Create Notes Fragment
    Design Create Note Fragment

---
+++ app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
@@ -0,0 +1,34 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
